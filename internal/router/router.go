144e6aa7d652dce1318ed7e8a44e9f0b8e0ebf5a
1tzme <zakir.itsme@gmail.com>
2025-07-04 23:07:42 +0500

    feat: add router for buckets, objects

---
+++ internal/router/router.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"net/http"
+
+	h "triple-s/internal/handlers"
+	s "triple-s/internal/structure"
+)
+
+func Router(server *s.Server) *http.ServeMux {
+	mux := http.NewServeMux()
+	h := h.NewHandler(server)
+
+	mux.HandleFunc("PUT /{BucketName}", h.PutBucket)
+	mux.HandleFunc("GET /", h.GetBuckets)
+	mux.HandleFunc("DELETE /{BucketName}", h.DeleteBucket)
+	mux.HandleFunc("PUT /{BucketName}/{ObjectKey}", h.PutObject)
+	mux.HandleFunc("GET /{BucketName}/{ObjectKey}", h.GetObject)
+	mux.HandleFunc("DELETE /{BucketName}/{ObjectKey}", h.DeleteObject)
+
+	return mux
+}
