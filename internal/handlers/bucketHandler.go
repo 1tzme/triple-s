01d83f27d2e2a609973ceaa207963e0a282b24f1
1tzme <zakir.itsme@gmail.com>
2025-07-06 00:24:49 +0500

    feat: add get buckets endpoint

--- internal/handlers/bucketHandler.go
+++ internal/handlers/bucketHandler.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"encoding/xml"
 	"net/http"
 
 	"triple-s/internal/storage"
+	"triple-s/internal/structure"
 	v "triple-s/internal/validator"
 )
 
@@ -33,4 +35,34 @@ func (h *Handler) PutBucket(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+func (h *Handler) GetBuckets(w http.ResponseWriter, r *http.Request) {
+	buckets, err := storage.ListBuckets(h.server.Dir)
+	if err != nil {
+		h.sendError(w, "InternalError", "Failed to list buckets", http.StatusInternalServerError)
+		return
+	}
+
+	response := structure.ListAllBuckets{
+		Owner: structure.Owner{
+			ID:          "",
+			DisplayName: "",
+		},
+		Buckets: structure.Buckets{
+			Bucket: make([]structure.Bucket, 0),
+		},
+	}
+
+	for i, bucket := range buckets {
+		response.Buckets.Bucket[i] = structure.Bucket{
+			Name:         bucket.Name,
+			CreationTime: bucket.CreationTime,
+		}
+	}
+
+	w.Header().Set("Content Type", "application/xml")
+	w.WriteHeader(http.StatusOK)
+
+	xml.NewEncoder(w).Encode(response)
 }
