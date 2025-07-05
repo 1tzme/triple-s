f99d3725b5ea8aff143ca8063998fb0fd557e9f7
1tzme <zakir.itsme@gmail.com>
2025-07-05 23:48:28 +0500

    feat: setup server init, error handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"triple-s/internal/router"
+	"triple-s/internal/structure"
+	v "triple-s/internal/validator"
+)
+
 func main() {
-	
+	port, dir, help := v.InitFlags()
+
+	if help {
+		v.PrintUsage()
+		return
+	}
+
+	err := os.MkdirAll(dir, 0o755)
+	if err != nil {
+		log.Fatalf("Failed to create directory %s: %v", dir, err)
+	}
+
+	server := structure.Server{
+		Dir:  dir,
+		Port: port,
+	}
+
+	mux := router.Router(&server)
+
+	fmt.Printf("Starting server on port %s, directory %s\n", port, dir)
+	err = http.ListenAndServe(":"+port, mux)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
