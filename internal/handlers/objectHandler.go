7ca919b2c63da8aed7a64b9b61b33437fd83965f
1tzme <zakir.itsme@gmail.com>
2025-07-06 23:38:13 +0500

    feat: add get object endpoint

--- internal/handlers/objectHandler.go
+++ internal/handlers/objectHandler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"strconv"
@@ -62,4 +63,48 @@ func (h *Handler) PutObject(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusOK)
+}
+
+func (h *Handler) GetObject(w http.ResponseWriter, r *http.Request) {
+	bucketName := r.PathValue("bucketName")
+	objectKey := r.PathValue("objectKey")
+
+	exists, err := storage.BucketExists(h.server.Dir, bucketName)
+	if err != nil {
+		h.sendError(w, "InternalError", "Failed to check bucket existence", http.StatusInternalServerError)
+		return
+	}
+	if !exists {
+		h.sendError(w, "NoSuchBucket", "The specified bucket does not exist", http.StatusNotFound)
+		return
+	}
+
+	objectExists, err := storage.ObjectExists(h.server.Dir, bucketName, objectKey)
+	if err != nil {
+		h.sendError(w, "InternalError", "Failed to check object existence", http.StatusInternalServerError)
+		return
+	}
+	if !objectExists {
+		h.sendError(w, "NoSuchKey", "The specified key does not exists", http.StatusNotFound)
+		return
+	}
+
+	object, err := storage.GetObjectMetadata(h.server.Dir, bucketName, objectKey)
+	if err != nil {
+		h.sendError(w, "InternalError", "Failed to get object metadata", http.StatusInternalServerError)
+		return
+	}
+
+	data, err := storage.GetObject(h.server.Dir, bucketName, objectKey)
+	if err != nil {
+		h.sendError(w, "InternalError", "Failed to read object", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", object.ContentType)
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", object.Size))
+	w.Header().Set("Last-Modified", object.LastModified.Format(http.TimeFormat))
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
 }
