d5ef470e0fdbed05e70169e81c92b3b725f8fd67
1tzme <zakir.itsme@gmail.com>
2025-07-07 16:19:19 +0500

    fix: corrected endpoint naming

--- internal/router/router.go
+++ internal/router/router.go
@@ -11,12 +11,12 @@ func Router(server *s.Server) *http.ServeMux {
 	mux := http.NewServeMux()
 	h := h.NewHandler(server)
 
-	mux.HandleFunc("PUT /{BucketName}", h.PutBucket)
+	mux.HandleFunc("PUT /{bucketName}", h.PutBucket)
 	mux.HandleFunc("GET /{$}", h.GetBuckets)
-	mux.HandleFunc("DELETE /{BucketName}", h.DeleteBucket)
-	mux.HandleFunc("PUT /{BucketName}/{ObjectKey}", h.PutObject)
-	mux.HandleFunc("GET /{BucketName}/{ObjectKey}", h.GetObject)
-	mux.HandleFunc("DELETE /{BucketName}/{ObjectKey}", h.DeleteObject)
+	mux.HandleFunc("DELETE /{bucketName}", h.DeleteBucket)
+	mux.HandleFunc("PUT /{bucketName}/{objectKey}", h.PutObject)
+	mux.HandleFunc("GET /{bucketName}/{objectKey}", h.GetObject)
+	mux.HandleFunc("DELETE /{bucketName}/{objectKey}", h.DeleteObject)
 
 	return mux
 }
